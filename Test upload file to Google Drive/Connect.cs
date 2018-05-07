a55329a19e50684e053c9140c2381403aa2df96c
Tấn Lưu Trọng <tanluutrong2206@gmail.com>
2018-05-07 11:04:31 +0700

    Seperate method to connect class. Add new method RetrieveFilePdfFolder

---
+++ Test upload file to Google Drive/Connect.cs
@@ -0,0 +1,154 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test_upload_file_to_Google_Drive
+{
+    public class Connect
+    {
+        private string[] Scopes = { DriveService.Scope.Drive };
+        public string FileId { get; set; }
+        public UserCredential GetAuthenication()
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = Environment.GetFolderPath(
+                    Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            return credential;
+        }
+
+        public List<Google.Apis.Drive.v3.Data.File> RetrieveAllFolders(DriveService service)
+        {
+            List<Google.Apis.Drive.v3.Data.File> result = new List<Google.Apis.Drive.v3.Data.File>();
+            FilesResource.ListRequest request = service.Files.List();
+
+            //setup filter only select folder
+            // cant use finding extension because in google drive docs file, powerpoint, excel, ... does not show extension
+            request.Q = "mimeType = 'application/vnd.google-apps.folder'";
+            
+            do
+            {
+                try
+                {
+                    FileList files = request.Execute();
+                    result.AddRange(files.Files);
+                    request.PageToken = files.NextPageToken;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occurred: " + e.Message);
+                    request.PageToken = null;
+                }
+            } while (!String.IsNullOrEmpty(request.PageToken));
+
+            return result;
+        }
+
+        public List<Google.Apis.Drive.v3.Data.File> RetrieveAllPdfFileDirectoryFolders(DriveService service, string parentId)
+        {
+            List<Google.Apis.Drive.v3.Data.File> result = new List<Google.Apis.Drive.v3.Data.File>();
+            FilesResource.ListRequest request = service.Files.List();
+
+            //setup filter only select folder
+            // cant use finding extension because in google drive docs file, powerpoint, excel, ... does not show extension
+            string filter = "mimeType = 'application/pdf' ";
+
+            if (!string.IsNullOrEmpty(parentId))
+            {
+                filter += $"and '{parentId}' in parents";
+                //request.Fields = $"'{parentId}' in parents";
+            }
+            request.Q = filter;
+
+            do
+            {
+                try
+                {
+                    FileList files = request.Execute();
+                    result.AddRange(files.Files);
+                    request.PageToken = files.NextPageToken;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occurred: " + e.Message);
+                    request.PageToken = null;
+                }
+            } while (!String.IsNullOrEmpty(request.PageToken));
+
+            return result;
+        }
+
+        public string CreateNewFolder(string folderContainId, DriveService driveService, string folderName)
+        {
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = folderName,
+                MimeType = "application/vnd.google-apps.folder",
+                Parents = new List<string> { folderContainId },
+            };
+            FilesResource.CreateRequest request;
+
+            request = driveService.Files.Create(fileMetadata);
+            Google.Apis.Drive.v3.Data.File file = request.Execute();
+
+            return file.Id;
+        }
+
+        public void CreateNewFile(string folderID, DriveService driveService, string filePath, string fileName)
+        {
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = fileName,
+
+                //find Mimetype corresponding with type of file inside below link
+                //https://developers.google.com/drive/v3/web/integrate-open#open_and_convert_google_docs_in_your_app
+                MimeType = "application/pdf",
+
+                //Choose what parent folder containt file
+                //Earch file/folder inside google drive has identify id
+                //Take that when reading all folder when start or after create new folder
+                Parents = new List<string> { folderID },
+            };
+
+            FilesResource.CreateMediaUpload request;
+
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                request = driveService.Files.Create(fileMetadata, stream, "application/pdf");
+                request.Fields = "id";
+                request.Upload();
+            }
+
+            //After create folder, get back folder to take folderID
+            var file = request.ResponseBody;
+            file.Shared = true;
+
+            //share link will has format like below
+            //https://drive.google.com/file/d/FILE_ID/view?usp=sharing
+            //Console.WriteLine($"File link: https://drive.google.com/file/d/{file.Id}/view?usp=sharing");
+
+            FileId = file.Id;
+        }
+    }
+}
--- Test upload file to Google Drive/Program.cs
+++ Test upload file to Google Drive/Program.cs
@@ -20,29 +20,13 @@ namespace Test_upload_file_to_Google_Drive
         // at ~/.credentials/drive-dotnet-quickstart.json
         // C:\Users\tanlu\Documents\.credentials\drive-dotnet-quickstart.json       this is my folder path
         //Reading https://developers.google.com/drive/v3/web/quickstart/dotnet to get more information
-
-        static string[] Scopes = { Google.Apis.Drive.v3.DriveService.Scope.Drive };
-        static string ApplicationName = "Drive API .NET Quickstart";
+        static string ApplicationName = "Funix's Certificate Generation Automatical";
 
         static void Main(string[] args)
         {
-            UserCredential credential;
+            Connect connect = new Connect();
 
-            using (var stream =
-                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
-            {
-                string credPath = System.Environment.GetFolderPath(
-                    System.Environment.SpecialFolder.Personal);
-                credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
-
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
-            }
+            UserCredential credential = connect.GetAuthenication();
 
             // Create Drive API service.
             var service = new DriveService(new BaseClientService.Initializer()
@@ -55,96 +39,21 @@ namespace Test_upload_file_to_Google_Drive
             service.HttpClient.Timeout = TimeSpan.FromMinutes(10);
 
             //get list of all folders
-            var files = retrieveAllFolders(service);
+            var files = new List<Google.Apis.Drive.v3.Data.File>();
+            //files = connect.RetrieveAllPdfFileDirectoryFolders(service, "1ebXLfc1SRpsp0uPZZ33pn9IJLCHbAYqa");
 
             //display to console
             foreach (var file in files)
             {
                 Console.WriteLine("{0} ({1})", file.Name, file.Id);
-                if (file.Name.Contains("MindMeister"))
-                {
-                    //CreateNewFolder(file.Id, service, "hello world");
-                    CreateNewFile(file.Id, service, "../../files/CC00706x-FUN180029-Kiều Hoàng Long.pdf", "CC00706x-FUN180029-Kiều Hoàng Long.pdf");
-                }
+                //if (file.Name.Contains("MindMeister"))
+                //{
+                //    string fileName = "SE00220x-FUN180037-Vũ Anh Tuấn.pdf";
+                //    //CreateNewFolder(file.Id, service, "hello world");
+                //    connect.CreateNewFile(file.Id, service, $"../../files/{fileName}", fileName);
+                //}
             }
             Console.Read();
-        }
-
-        public static List<Google.Apis.Drive.v3.Data.File> retrieveAllFolders(DriveService service)
-        {
-            List<Google.Apis.Drive.v3.Data.File> result = new List<Google.Apis.Drive.v3.Data.File>();
-            Google.Apis.Drive.v3.FilesResource.ListRequest request = service.Files.List();
-
-            //setup filter only select folder
-            // cant use finding extension because in google drive docs file, powerpoint, excel, ... does not show extension
-            request.Q = "mimeType = 'application/vnd.google-apps.folder'";
-
-            do
-            {
-                try
-                {
-                    FileList files = request.Execute();
-                    result.AddRange(files.Files);
-
-                    request.PageToken = files.NextPageToken;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("An error occurred: " + e.Message);
-                    request.PageToken = null;
-                }
-            } while (!String.IsNullOrEmpty(request.PageToken));
-
-            return result;
-        }
-
-        internal static void CreateNewFolder(string folderID, DriveService driveService, string folderName)
-        {
-            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
-            {
-                Name = folderName,
-                MimeType = "application/vnd.google-apps.folder",
-                Parents = new List<string> { folderID },
-                //Size = 28000,
-            };
-            FilesResource.CreateRequest request;
-
-            request = driveService.Files.Create(fileMetadata);
-            Google.Apis.Drive.v3.Data.File file = request.Execute();
-
-
-            Console.WriteLine("File ID: " + file.Id);
-        }
-
-        internal static void CreateNewFile(string folderID, DriveService driveService, string filePath, string fileName)
-        {
-            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
-            {
-                Name = fileName,
-
-                //find Mimetype corresponding with type of file inside below link
-                //https://developers.google.com/drive/v3/web/integrate-open#open_and_convert_google_docs_in_your_app
-                MimeType = "application/pdf",
-
-                //Choose what parent folder containt file
-                //Earch file/folder inside google drive has identify id
-                //Take that when reading all folder when start or after create new folder
-                Parents = new List<string> { folderID },
-            };
-
-            FilesResource.CreateMediaUpload request;
-
-            using (var stream = new System.IO.FileStream(filePath, System.IO.FileMode.Open))
-            {
-                request = driveService.Files.Create(fileMetadata, stream, "application/pdf");
-                request.Fields = "id";
-                request.Upload();
-            }
-
-            //After create folder, get back folder to take folderID
-            var file = request.ResponseBody;
-
-            Console.WriteLine("File ID: " + file.Id);
         }
     }
 }
